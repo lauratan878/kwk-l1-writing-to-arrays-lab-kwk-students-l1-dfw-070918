42afb307f7dd238f8b6c6d29641daf16b0c5f584
lauratan878 <lauratan878@gmail.com>
2018-07-10 20:59:12 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -3,11 +3,30 @@ rainbow_colors = [
   "default",
   "light_cyan"
 ]
+# [
+#   "yellow",
+#   "default",
+#   "light_cyan"]
+#   puts room[0]="red"
+#   puts room[1]="light_red"
+#   puts room[2]="light_yellow"
+#   room<<"yellow"
+#   room<<"default"
+#   room<<"light_cyan"
 
 def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+  array= []
+  array[0]="red"
+  array[1]="light_red"
+  array[2]="light_yellow"
+  array
 end
+
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  array = []
+  add_colors << "green"
+  add_colors << "blue"
 end
